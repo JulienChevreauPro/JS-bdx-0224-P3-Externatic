d447abc5f0b502e8ca974d0811790cc8f683937c
JulienChevreauPro <julienchevreau33@gmail.com>
2024-09-03 11:22:10 +0200

    delete useless service and fix middlewares

--- server/app/middlewares/verifyAuthCurrent.js
+++ server/app/middlewares/verifyAuthCurrent.js
@@ -2,20 +2,27 @@ const jwt = require("jsonwebtoken");
 
 const verifyAuthCurrent = (req, res, next) => {
   const authorizationHeader = req.get("Authorization");
-  if (authorizationHeader) {
-    const [type, token] = authorizationHeader.split(" ");
-    if (type === "Bearer" && token) {
-      try {
-        const decodedToken = jwt.verify(token, process.env.APP_SECRET);
-        req.auth = {
-          role: decodedToken.role,
-          id: decodedToken.id,
-        };
-      } catch (err) {
-        return res.status(401).json({ error: "Token invalide" });
-      }
-    }
+
+  if (!authorizationHeader) {
+    return res.status(401).json({ error: "Authorization header missing" });
   }
-  return next();
+
+  const [type, token] = authorizationHeader.split(" ");
+
+  if (type !== "Bearer" || !token) {
+    return res.status(401).json({ error: "Invalid authorization format" });
+  }
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.APP_SECRET);
+    req.auth = {
+      role: decodedToken.role,
+      id: decodedToken.id,
+    };
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
 };
+
 module.exports = { verifyAuthCurrent };
